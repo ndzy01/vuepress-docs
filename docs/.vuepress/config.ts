991a8718b80acda821e66d022e3236a557d8148d
张一 <2326962137@qq.com>
2022-07-10 11:11:44 +0800

    chore 更新配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig, defaultTheme } from "vuepress";
 import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 export default defineUserConfig({
+  base: "/",
   lang: "zh-CN",
   title: "docs",
   description: "文档",
